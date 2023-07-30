509be732a26627f2ede4916d7f7212de086cc095
Artem-Shabaltun <134101240+Artem-Shabaltun@users.noreply.github.com>
2023-07-30 14:27:26 +0300

    reminder of collegues birthdays v1.0.1
    Fixed start of the week. 0 = Monday, 6 = Sunday

--- birthdays_reminder.py
+++ birthdays_reminder.py
@@ -4,29 +4,36 @@ def get_birthdays_per_week(users):
 
     birthdays = [[] for _ in range(7)] # створимо список та припишемо іменинників до кожного дня тижня, що припаде на день народження
 
+    today = datetime.now().date() #визначимо поточну дату
+
     for user in users:
         birthday = user["birthday"].date() 
-        birthday_day_of_week = birthday.weekday() # Отримаємо поточний день тижня (0 - понеділок, 6 - неділя)
+        birthday_day_of_week = birthday.weekday() # визначає понеділок першим днем тижня 0 = понеділок, 6 = неділя
+        monday = today - timedelta(days=today.weekday()) #від поточної дати виконуємо повернення на понеділок, щоб не збивалося визначення 0 = понеділок
 
-        if birthday_day_of_week >=5: #Перевіримо, даний день є суботою, чи неділею
+        if birthday_day_of_week >=5: #проводимо перевірку чи даний день є вихідним (5 = субота, 6 = неділя)
             need_days_to_add = 2 if birthday_day_of_week == 5 else 1 #Визначаємо скільки потрібно днів, аби перенести привітання на понеділок, в разі, якщо це вихідний
             birthday = birthday + timedelta(days=need_days_to_add)
 
         birthdays[birthday.weekday()].append(user["name"]) #Додаємо ім'я до списку іменинників на відповідний день, який ми створили
 
     for i in range(7):
-        date_of_birthday = datetime.now() + timedelta(i)
+        date_of_birthday = monday + timedelta(days=i)
         if birthdays[i]:
             print(f"{date_of_birthday.strftime('%A')}: {', '.join(birthdays[i])}")
 
 users = [
-    {"name": "Bill", "birthday": datetime(2023, 7, 30)},
-    {"name": "Jill", "birthday": datetime(2023, 7, 31)},
-    {"name": "Kim", "birthday": datetime(2023, 8, 1)},
-    {"name": "Jan", "birthday": datetime(2023, 8, 2)},
-    {"name": "Ivan", "birthday": datetime(2023, 8, 3)},
-    {"name": "Nick", "birthday": datetime(2023, 8, 4)},
-    {"name": "Artem", "birthday": datetime(2023, 8, 3)},
+    {"name": "Bill", "birthday": datetime(2023, 7, 30)}, # Переноситься на понеділок
+    {"name": "Jill", "birthday": datetime(2023, 7, 31)}, # Понеділок
+    {"name": "Kim", "birthday": datetime(2023, 8, 1)}, # Вівторок
+    {"name": "Alina", "birthday": datetime(2023, 8, 2)}, # Середа /Вівторок (перевірити чи зникне пуста середа в списку)
+    {"name": "Jan", "birthday": datetime(2023, 8, 2)}, # Середа /Вівторок (перевірити чи зникне пуста середа в списку)
+    {"name": "Mykola", "birthday": datetime(2023, 8, 1)}, # Вівторок
+    {"name": "Ivan", "birthday": datetime(2023, 8, 3)}, # Четвер
+    {"name": "Oles", "birthday": datetime(2023, 8, 3)}, # Четвер
+    {"name": "Nick", "birthday": datetime(2023, 8, 4)}, # П'ятниця
+    {"name": "Artem", "birthday": datetime(2023, 8, 4)}, # П'ятниця
+    {"name": "Mykyta", "birthday": datetime(2023, 8, 1)}, # Понеділок
 ]
 
 get_birthdays_per_week(users)
