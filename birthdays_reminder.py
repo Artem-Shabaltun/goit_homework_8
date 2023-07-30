1c71ca991ab94f8ec893c370b81af5961d13df8a
Artem-Shabaltun <134101240+Artem-Shabaltun@users.noreply.github.com>
2023-07-30 20:20:16 +0300

    reminder of collegues birthdays v1.0.2
    Updated per comments. Used the data filter, not a day of the week. Added compatibility with various date formats

--- birthdays_reminder.py
+++ birthdays_reminder.py
@@ -1,39 +1,82 @@
 from datetime import datetime, timedelta
 
-def get_birthdays_per_week(users):
 
-    birthdays = [[] for _ in range(7)] # створимо список та припишемо іменинників до кожного дня тижня, що припаде на день народження
+def get_period(next_week_start:bool) -> dict[int,datetime]: # створюємо словник із датами наступних 7 днів тижня
+    next_days = []
+    curent_date = datetime.now()
+    
+    if next_week_start: # визначаємо дату, з якої почненться період для виводу іменин
+        period_to_start = curent_date + timedelta(days=4-curent_date.weekday())  # від найближчого понеділка
+    else:
+        period_to_start = curent_date # відсьогодні, якщо сьогодні  понеділок
 
-    today = datetime.now().date() #визначимо поточну дату
+    count = 1
 
-    for user in users:
-        birthday = user["birthday"].date() 
-        birthday_day_of_week = birthday.weekday() # визначає понеділок першим днем тижня 0 = понеділок, 6 = неділя
-        monday = today - timedelta(days=today.weekday()) #від поточної дати виконуємо повернення на понеділок, щоб не збивалося визначення 0 = понеділок
+    while count <= 7:
+        period_to_start += timedelta(1)
+        next_days.append((period_to_start.weekday(), period_to_start))
+        count += 1
 
-        if birthday_day_of_week >=5: #проводимо перевірку чи даний день є вихідним (5 = субота, 6 = неділя)
-            need_days_to_add = 2 if birthday_day_of_week == 5 else 1 #Визначаємо скільки потрібно днів, аби перенести привітання на понеділок, в разі, якщо це вихідний
-            birthday = birthday + timedelta(days=need_days_to_add)
+    return dict(next_days)
 
-        birthdays[birthday.weekday()].append(user["name"]) #Додаємо ім'я до списку іменинників на відповідний день, який ми створили
 
-    for i in range(7):
-        date_of_birthday = monday + timedelta(days=i)
-        if birthdays[i]:
-            print(f"{date_of_birthday.strftime('%A')}: {', '.join(birthdays[i])}")
+def get_datetime_date(date: datetime|str) -> datetime: # передбачимо використання різних форматів дати
+    if isinstance(date, str):
+        if date.find("-") == 4:
+            return datetime.strptime(date, "%Y-%m-%d")
+        elif date.find("-") == 2:
+            return datetime.strptime(date, "%d-%m-%Y")
+        elif date.find(".") == 4:
+            return datetime.strptime(date, "%Y.%m.%d")
+        elif date.find(".") == 2:
+            return datetime.strptime(date, "%d.%m.%Y")
+        elif date.find("/") == 4:
+            return datetime.strptime(date, "%Y/%m/%d")
+        elif date.find("/") == 2:
+            return datetime.strptime(date, "%d/%m/%Y")
+        else:
+            return datetime(1, 1, 1)
+    else:
+        return date
+    
+
+def get_birthdays_per_week(users: list[dict[str,datetime|str]], next_week_start:bool=True) -> None: # виводимо імена користувачів, які мають день народження , починаючи з понеділка
+    weekdays = get_period(next_week_start)
+    
+    moove_users_lst = [] # Створюємо список. Якщо день народження випадає на вихіний - іменинники додаються до цього списку
+    
+    for weekday_int, weekday_datetime in weekdays.items():
+        users_lst = [] # Створюємо список. Для будніх днів - іменинники потрапляють в цей список
+        
+        for user_data in users:
+            user_name = user_data.get("name")
+            user_birthday = get_datetime_date(user_data.get("birthday"))
+            
+            if user_birthday.month == weekday_datetime.month and user_birthday.day == weekday_datetime.day:
+                if weekday_int in (5, 6):
+                    moove_users_lst.append(user_name)
+                else:
+                    users_lst.append(user_name)
+        
+        if weekday_int == 0:
+            users_lst.extend(moove_users_lst)
+        
+        if len(users_lst):
+            weekday_str = weekday_datetime.strftime("%A")
+            users_str = ", ".join(users_lst)
+            print(f"{weekday_str}: {users_str}")
 
 users = [
-    {"name": "Bill", "birthday": datetime(2023, 7, 30)}, # Переноситься на понеділок
-    {"name": "Jill", "birthday": datetime(2023, 7, 31)}, # Понеділок
-    {"name": "Kim", "birthday": datetime(2023, 8, 1)}, # Вівторок
-    {"name": "Alina", "birthday": datetime(2023, 8, 2)}, # Середа /Вівторок (перевірити чи зникне пуста середа в списку)
-    {"name": "Jan", "birthday": datetime(2023, 8, 2)}, # Середа /Вівторок (перевірити чи зникне пуста середа в списку)
-    {"name": "Mykola", "birthday": datetime(2023, 8, 1)}, # Вівторок
-    {"name": "Ivan", "birthday": datetime(2023, 8, 3)}, # Четвер
-    {"name": "Oles", "birthday": datetime(2023, 8, 3)}, # Четвер
-    {"name": "Nick", "birthday": datetime(2023, 8, 4)}, # П'ятниця
-    {"name": "Artem", "birthday": datetime(2023, 8, 4)}, # П'ятниця
-    {"name": "Mykyta", "birthday": datetime(2023, 8, 1)}, # Понеділок
+    {"name": "Bill", "birthday": datetime(1997, 7, 30)}, # Переноситься на понеділок
+    {"name": "Jill", "birthday": datetime(1997, 7, 31)}, # Понеділок
+    {"name": "Kim", "birthday": datetime(1997, 8, 1)}, # Вівторок
+    {"name": "Alina", "birthday": datetime(1997, 8, 2)}, # Середа
+    {"name": "Jan", "birthday": datetime(1997, 8, 2)}, # Середа
+    {"name": "Mykola", "birthday": datetime(1997, 8, 1)}, # Вівторок
+    {"name": "Ivan", "birthday": datetime(1997, 8, 3)}, # Четвер
+    {"name": "Oles", "birthday": datetime(1997, 8, 3)}, # Четвер
+    {"name": "Nick", "birthday": datetime(1997, 8, 4)}, # П'ятниця
+    {"name": "Artem", "birthday": datetime(1997, 8, 4)}, # П'ятниця
 ]
-
+     
 get_birthdays_per_week(users)
